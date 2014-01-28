182a1823c820a48649fa49d34a6e13ccca67238d
Adrian Cowell <me@adrianc.info>
2014-01-28 11:25:02 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IpadEdu::Application.config.secret_key_base = 'eaf13af9ed74cdc3f9ac2da3fda162cdb8cea88f3350bb35842b0738c94d75a58881bc1d6153b85245b452f1cfabbf6eb8c87246f7a0e3e204e968ff32738129'
